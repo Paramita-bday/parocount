e216a85e51570de53b13e6cdeb5129ea210ef463
Sougata <64636760+Sougata1119@users.noreply.github.com>
2025-01-16 08:22:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -95,6 +95,25 @@ function displayCountdownEnded() {
     }, 3000);
 }
 
+// Google Tag (gtag.js)
+(function() {
+    // Dynamically create the script tag for gtag.js
+    var gtagScript = document.createElement('script');
+    gtagScript.async = true;
+    gtagScript.src = "https://www.googletagmanager.com/gtag/js?id=G-720BWJCGW9";
+    document.head.appendChild(gtagScript);
+  
+    // Initialize dataLayer and gtag function
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    window.gtag = gtag;
+  
+    // Set the initial gtag configuration
+    gtag('js', new Date());
+    gtag('config', 'G-720BWJCGW9');
+  })();
+
+  
 // Music play button functionality
 const playMusicButton = document.getElementById("playMusicButton");
 const music = document.getElementById("backgroundMusic");
